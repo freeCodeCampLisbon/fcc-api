7d64555f70b362b3ba71f429d5fa2b2bdafcd902
joão Petinga <joao.petinga@gmail.com>
2021-01-10 04:38:13 +0000

    make sure user cant reserve a seat twice for the same event

--- src/controllers/MeetupController.ts
+++ src/controllers/MeetupController.ts
@@ -1,35 +1,55 @@
 import faunadb from "faunadb";
 import Constants from "../constants";
-import Mailchimp from '../plugins/mailchimp'
+import Mailchimp from "../plugins/mailchimp";
 const client = new faunadb.Client({ secret: `${Constants.faunaDbKey}` });
-const { Create, Collection } = faunadb.query;
+const { Create, Collection, Match, Get, Index } = faunadb.query;
 export default {
   async store(req: any, res: any) {
     try {
-    
       const { email, uid, date, sub_newsletter } = req.body;
-      var meetupId = uid
-       await client.query(
+      var meetupId = uid;
+
+      // Check if user has already reserved their seat
+      try {
+        await client.query(
+          Get(Match(Index("meetup_users_by_uid_and_email"), [email, uid]))
+        );
+        res.status(409).json({
+          message: "You've already reserved your seat for this meetup",
+          type: 'warning'
+        });
+        return;
+      } catch (error) {
+        console.log("error MeetupController: ", error);
+      }
+
+      await client.query(
         Create(Collection("meetups"), {
           data: { uid, email, date },
         })
       );
-      if(sub_newsletter) {
+      if (sub_newsletter) {
+       
         try {
-          await Mailchimp.subNewsletter('', email)
+          await Mailchimp.subNewsletter("", email);
         } catch (error) {
-            console.log('here')
-             res.json({message: 'Your seat has been reserved, thank you!. You were already subscribed to newsletter'});
-             return;
+         
+          res.json({
+            message:
+              "Your seat has been reserved, thank you!. You were already subscribed to newsletter",
+            type: 'success'
+          });
+          return;
         }
       }
-      res.json({message: 'Your seat has been reserved, thank you!'});
-
+      res.json({ message: "Your seat has been reserved, thank you!", type: 'success' });
     } catch (error) {
-      console.log('error: ', error)
-      res.status(401).json({message: `We could not fulfill your request at the moment. If the problem persists, please contact info@freecodecamplisbon.org to reserve your seat and add the subject "Seat Reservation for ${meetupId} meetup" to your email.`})
-      console.log('error MeetupController@store: ', error.requestResult.statusCode)
-      
+      console.log("error: ", error);
+      res.status(401).json({
+        message: `We could not fulfill your request at the moment. If the problem persists, please contact info@freecodecamplisbon.org to reserve your seat and add the subject "Seat Reservation for ${meetupId} meetup" to your email.`,
+        type: 'danger'
+      });
+     
     }
   },
 };
